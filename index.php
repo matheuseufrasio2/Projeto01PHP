f334f9833808364e5f07a73aa3cae51616ebe7e1
Matheus Eufrasio <matheuseufrasio2@gmail.com>
2019-10-23 19:29:58 -0300

    Finzalizando toda a parte de tag do site

---
+++ index.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Projeto 01</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="keywords" content="palavras,chave,do,meu,site">
+	<meta name="description" content="descricao do meu website">
+	<meta charset="utf-8">
+</head>
+<body>
+	<!--Inicio da Header-->
+	<header>
+		<div class="center">
+			<div class="logo">Logomarca</div>
+			<nav class="desktop">
+				<ul>
+					<li><a href="">Home</a></li>
+					<li><a href="">Sobre</a></li>
+					<li><a href="">Serviços</a></li>
+					<li><a href="">Contato</a></li>
+				</ul>
+				
+			</nav>
+
+			<nav class="mobile">
+				<ul>
+					<li><a href="">Home</a></li>
+					<li><a href="">Sobre</a></li>
+					<li><a href="">Serviços</a></li>
+					<li><a href="">Contato</a></li>
+				</ul>
+				
+			</nav>
+		</div class="center">
+	</header>
+	<!--Seção de banner-->
+	<section class="banner-principal">
+		<div class="center">
+			<form>
+				<h2>Qual o seu melhor E-mail?</h2>
+				<input type="email" name="email" required>
+				<input type="submit" name="acao" value="Cadastrar!">
+			</form>
+		</div class="center">
+	</section>
+	<!--Descrição do Autor-->
+	<section class="descricao-autor">
+		<div class="center">
+			<div class="W50">
+				<h2>Matheus Eufrásio</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur commodo elit eu libero ultricies imperdiet. Aenean et turpis velit. Sed aliquet, enim iaculis dictum semper, dui justo convallis odio, sit amet rutrum justo sapien sit amet ex. Praesent iaculis malesuada posuere. Donec at purus id lacus mollis pretium sed et leo. Etiam mollis vestibulum orci non varius. Sed nec venenatis lectus.</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur commodo elit eu libero ultricies imperdiet. Aenean et turpis velit. Sed aliquet, enim iaculis dictum semper, dui justo convallis odio, sit amet rutrum justo sapien sit amet ex. Praesent iaculis malesuada posuere. Donec at purus id lacus mollis pretium sed et leo. Etiam mollis vestibulum orci non varius. Sed nec venenatis lectus.</p>
+			</div>
+			<div class="W50">
+				<!--Pegar imagem depois-->
+				<img src="images/pic2.jpg">
+			</div>
+		</div class="center">
+	</section>
+	<!--Especialidades-->
+	<section class="especialidades">
+		<div class="center">
+			<h2>Especialidades</h2>
+			<div class="box-especialidade">
+				<h3>Icone 1</h3>
+				<h3>CSS3</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur commodo elit eu libero ultricies imperdiet. Aenean et turpis velit. Sed aliquet</p>
+			</div>
+			<div class="box-especialidade">
+				<h3>Icone 2</h3>
+				<h3>HTML5</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur commodo elit eu libero ultricies imperdiet. Aenean et turpis velit. Sed aliquet</p>
+			</div>
+			<div class="box-especialidade">
+				<h3>Icone 3</h3>
+				<h3>JAVASCRIPT</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur commodo elit eu libero ultricies imperdiet. Aenean et turpis velit. Sed aliquet</p>
+			</div>
+		</div class="center">
+	</section>
+
+	<!--Serviços e depoimentos-->
+	<section class="extras">
+
+		<div class="center">
+			<div class="W50">
+				<h2 class="title">Depoimentos</h2>
+				<div class="depoimento-single">
+					<p class="depoimento-descricao">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur commodo elit eu libero ultricies imperdiet. Aenean et turpis velit. Sed aliquet, enim iaculis dictum semper, dui justo convallis odio</p>
+					<p class="nome-autor">Larissa Eufrásio</p>
+
+					<div class="depoimento-single">
+					<p class="depoimento-descricao">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur commodo elit eu libero ultricies imperdiet. Aenean et turpis velit. Sed aliquet, enim iaculis dictum semper, dui justo convallis odio</p>
+					<p class="nome-autor">Igor Rafael</p>
+
+					<div class="depoimento-single">
+					<p class="depoimento-descricao">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur commodo elit eu libero ultricies imperdiet. Aenean et turpis velit. Sed aliquet, enim iaculis dictum semper, dui justo convallis odio</p>
+					<p class="nome-autor">Rodrigo Verçosa</p>
+				</div> <!--depoimento-single-->
+
+			</div> <!--W50-->
+
+
+			<div class="W50">
+				<h2 class="title">Serviços</h2>
+				<div class="servicos">
+					<ul>
+					<li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur commodo elit eu libero ultricies imperdiet. Aenean et turpis velit.</li>
+					<li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur commodo elit eu libero ultricies imperdiet. Aenean et turpis velit.</li>
+					<li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur commodo elit eu libero ultricies imperdiet. Aenean et turpis velit.</li>
+					</ul>
+				</div><!--servicos-->
+				
+
+			</div> <!--W50-->
+			
+		</div> <!--center-->
+		
+	</section> <!--extras-->
+
+	<footer>
+		<div class="center">
+			<p>Todos os direitos reservados</p>	
+		</div><!--center-->
+		
+	</footer>
+
+</body>
+</html>
